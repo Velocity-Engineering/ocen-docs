78a96be000aa707525a5b7b7df436388884e8f41
Adittya Dey <ichbinadittyadey@gmail.com>
2023-07-10 12:03:58 +0530

    fix: base url dor docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,10 +9,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://ispirt.github.io',
+  url: 'https://velocity-engineering.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/ocen-docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
